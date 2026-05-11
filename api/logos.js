ccda5646bd42b003722e7dfac7a27036aa052f54
betterbookie2026-collab <betterbookie2026@gmail.com>
2026-05-11 19:56:52 -0400

    Update logos.js

--- api/logos.js
+++ api/logos.js
@@ -14,6 +14,10 @@ export default async function handler(req, res) {
     nba: 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams',
     mlb: 'https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams',
     nhl: 'https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/teams',
+    ncaafb: 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/teams?limit=200',
+    ncaabb: 'https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams?limit=200',
+    ncaasb: 'https://site.api.espn.com/apis/site/v2/sports/baseball/college-baseball/teams?limit=200',
+    ncaasoftball: 'https://site.api.espn.com/apis/site/v2/sports/softball/college-softball/teams?limit=200',
   };
 
   const url = urls[sport] || urls.nfl;
@@ -25,7 +29,14 @@ export default async function handler(req, res) {
       name: t.team.displayName,
       logo: t.team.logos?.[0]?.href ?? '',
       abbreviation: t.team.abbreviation,
+      rank: t.team.rank ?? 999,
     }));
+
+    // Sort by rank for college sports so top teams appear first
+    if (['ncaafb', 'ncaabb', 'ncaasb', 'ncaasoftball'].includes(sport)) {
+      teams.sort((a, b) => a.rank - b.rank);
+    }
+
     res.status(200).json(teams);
   } catch (err) {
     res.status(500).json({ error: err.message });
