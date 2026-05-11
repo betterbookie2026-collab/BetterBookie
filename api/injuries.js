536bc45753baddf300e0cb00f48055ffb0c42d25
betterbookie2026-collab <betterbookie2026@gmail.com>
2026-05-11 16:06:32 -0400

    Create injuries.js

---
+++ api/injuries.js
@@ -0,0 +1,31 @@
+export default async function handler(req, res) {
+  const { sport = 'nfl' } = req.query;
+
+  const leagueMap = {
+    nfl: { league: 1, season: 2024 },
+    nba: { league: 12, season: 2024 },
+    mlb: { league: 1, season: 2024 },
+  };
+
+  const { league, season } = leagueMap[sport] || leagueMap.nfl;
+
+  const baseUrls = {
+    nfl: 'https://v1.american-football.api-sports.io',
+    nba: 'https://v2.nba.api-sports.io',
+    mlb: 'https://v1.baseball.api-sports.io',
+  };
+
+  const baseUrl = baseUrls[sport] || baseUrls.nfl;
+
+  const response = await fetch(
+    `${baseUrl}/injuries?league=${league}&season=${season}`,
+    {
+      headers: {
+        'x-apisports-key': process.env.APISPORTS_KEY,
+      },
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
